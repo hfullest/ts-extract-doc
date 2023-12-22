1b277e75e08df707523de15806f4b11311914630
liuhuaifu <huafull0578@163.com>
2023-12-22 17:55:00 +0800

    feat: 完善example

--- example/index.ts
+++ example/index.ts
@@ -3,10 +3,10 @@ import { extractTsToMarkdown } from "../src"; // 引入库
 import { resolve } from "path";
 
 
-const path = resolve(__dirname, './demo.tsx');
+const path = resolve(process.cwd(), './demo.tsx');
 
 const result = extractTsToMarkdown(path, {
-    document: { strategy: 'manual' },
+    // document: { strategy: 'manual' }, // 项目里可以使用手动指定，这里为了方便，对所有导出都进行文档输出
     markdown: { template: "beauty" },
 });
 
